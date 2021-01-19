1ce76329c88c08726c8a5267cd193669da79bee4
Sled <angelherxd@hotmail.com>
2021-01-18 21:38:23 -0700

    better openresty structure, finished packages index

--- openresty/lua/main.lua
+++ openresty/lua/main.lua
@@ -1,11 +1,2 @@
-local json = require "cjson"
 local template = require "resty.template"
-
-local f = io.open("packagesList.json", "r")
-local jsonContent = f:read("*all")
-
-local packages = json.decode(jsonContent)
-
-template.render("packagesIndex.html", {
-    packages = packages
-})
+template.render("main.html")

ba12d0e1d525153ffb5e6048c94d58762eb32aa6
Sled <angelherxd@hotmail.com>
2021-04-06 03:24:43 -0600

    fix packages list search path

--- openresty/lua/packages.lua
+++ openresty/lua/packages.lua
@@ -1,7 +1,7 @@
 local json = require "cjson"
 local template = require "resty.template"
 
-local f = io.open("packagesList.json", "r")
+local f = io.open(ngx.config.prefix() .. "packagesList.json", "r")
 local jsonContent = f:read("*all")
 
 local packages = json.decode(jsonContent)

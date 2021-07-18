c0df9356ee36ffdfb1d3c37998f0c5b8af8f8b53
Sled <angelherxd@hotmail.com>
2021-07-18 02:01:23 -0600

    minor merc tests changes

--- tests/modules/merc.lua
+++ tests/modules/merc.lua
@@ -12,35 +12,34 @@ local merc = require "Mercury.modules.merc"
 
 testMerc = {}
 
+-- Before every unit test
 function testMerc:setUp()
-    -- Before every unit test
-    self.inputMerc = "./Mercury/tests/modules/merc/package/unpack.merc"
-    self.outputMercPath = "./Mercury/tests/modules/merc/unpack"
+    self.inputPack = "./Mercury/tests/modules/merc/unpack/unpack.merc"
+    self.unpackDir = "./Mercury/tests/modules/merc/unpackDir"
     
-    self.packDir = "./Mercury/tests/modules/merc/packDir"
-    self.outputDir = "./Mercury/tests/modules/merc/package/pack.merc"
+    self.packDir = "./Mercury/tests/modules/merc/pack"
+    self.outputPack = "./Mercury/tests/modules/merc/pack/pack.merc"
 end
 
 -- Specifically test unpack method
 function testMerc:testUnpack()
-    --IsDebugModeEnabled = true
-    delete(self.outputMercPath, true)
-    createFolder(self.outputMercPath)
-    if (exist(self.inputMerc)) then
-        lu.assertIsTrue(merc.unpack(self.inputMerc, self.outputMercPath))
-        lu.assertIsTrue(exist(self.outputMercPath .. "/manifest.json"))
-        delete(self.outputMercPath, true)
+    delete(self.unpackDir, true)
+    createFolder(self.unpackDir)
+    if (exist(self.inputPack)) then
+        lu.assertIsTrue(merc.unpack(self.inputPack, self.unpackDir))
+        lu.assertIsTrue(exist(self.unpackDir .. "/manifest.json"))
+        delete(self.unpackDir, true)
     else
         lu.fail("Input merc package does not exist")
     end
 end
 
 function testMerc:testPack() 
-    delete(self.outputDir)
+    delete(self.outputPack)
     if(exist(self.packDir)) then
-        lu.assertIsTrue(merc.pack(self.packDir, self.outputDir))
-        lu.assertIsTrue(exist(self.outputDir))
-        delete(self.outputDir)
+        lu.assertIsTrue(merc.pack(self.packDir, self.outputPack))
+        lu.assertIsTrue(exist(self.outputPack))
+        delete(self.outputPack)
     else
         lu.fail("packDir does not exist")
     end

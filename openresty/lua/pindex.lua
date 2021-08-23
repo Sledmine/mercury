a48272d04fc5c851d337f64696430b54bf0f0a3a
Sledmine <sledmine@gmail.com>
2021-08-23 17:26:26 -0600

    remove merge leftovers

--- openresty/lua/pindex.lua
+++
@@ -1,7 +0,0 @@
-local json = require "cjson"
-local template = require "resty.template"
-
-local f = io.open(ngx.config.prefix() .. "packagesList.json", "r")
-local jsonContent = f:read("*all")
-
-ngx.say(jsonContent)

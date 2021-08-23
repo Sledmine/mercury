988ee54211f4a50b14125a1b3af82344044f33af
Sled <angelherxd@hotmail.com>
2021-08-22 18:14:27 -0600

    update repo host and version number

--- modules/constants.lua
+++ modules/constants.lua
@@ -1,12 +1,12 @@
 local constants = {}
 
-constants.mercuryVersion = "2.0.0-beta"
+constants.mercuryVersion = "2.0.0"
 constants.xd3CmdLine = "xdelta3 -d -s \"%s\" \"%s\" \"%s\""
 constants.latestReleaseApi = "https://api.github.com/repos/Sledmine/Mercury/releases/latest"
 constants.gitHubReleases = "https://github.com/Sledmine/Mercury/releases/{tagName}/"
 constants.mercuryWeb = "http://mercury.shadowmods.net"
 constants.packageIndex = constants.mercuryWeb .. "/pindex"
-constants.repositoryHost = "genesis.vadam.net"
+constants.repositoryHost = "genesis.shadowmods.net"
 constants.vulcanoPath = "api/vulcano"
 constants.mapRepositoryDownload = "https://storage.shadowmods.net/public/haloce/hac2_repo/%s.zip"
 

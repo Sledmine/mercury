6eeaae8ac7925be79d31c88b8d893b7514d4f3e9
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-10-28 20:49:02 -0600

    add new api definitions, maps repo -> hac2

--- modules/constants.lua
+++ modules/constants.lua
@@ -1,13 +1,12 @@
 local constants = {}
 
-constants.mercuryVersion = "2.0.1"
+constants.mercuryVersion = "3.0.0"
 constants.xd3CmdLine = "xdelta3 -d -s \"%s\" \"%s\" \"%s\""
 constants.latestReleaseApi = "https://api.github.com/repos/Sledmine/Mercury/releases/latest"
-constants.gitHubReleases = "https://github.com/Sledmine/Mercury/releases/{tagName}/"
+constants.gitHubReleases = "https://github.com/Sledmine/Mercury/releases/%s/"
 constants.mercuryWeb = "http://mercury.shadowmods.net"
-constants.packageIndex = constants.mercuryWeb .. "/pindex"
 constants.repositoryHost = "genesis.shadowmods.net"
-constants.vulcanoPath = "api/vulcano"
-constants.mapRepositoryDownload = "https://storage.shadowmods.net/public/haloce/hac2_repo/%s.zip"
+--constants.vulcanoPath = ""
+constants.mapRepositoryDownload = "http://maps.halonet.net/maps/%s.zip"
 
 return constants

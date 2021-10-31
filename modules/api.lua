987184fcde1c44d31c9b828a8c30fe94014c4c28
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-10-31 02:46:45 -0700

    new constants, new api definitions

--- modules/api.lua
+++ modules/api.lua
@@ -13,6 +13,17 @@ api.protocol = "https"
 api.repositoryHost = constants.repositoryHost
 api.version = "v1"
 
+---@class packageMetadata
+---@field name string
+---@field label string
+---@field author string
+---@field version string
+---@field internalVersion string
+---@field category string
+---@field conflicts string[]
+---@field mirrors string[]
+---@field nextVersion string
+
 --- Generate an URL using api definitions
 local function vulcanoUrl()
     return api.protocol .. "://" .. api.repositoryHost .. "/" .. api.version
@@ -25,7 +36,7 @@ end
 
 --- Simple GET HTTP method
 local function get(url)
-    --local result, error, headers, status, response = fdownload.get(url)
+    -- local result, error, headers, status, response = fdownload.get(url)
     local response = requests.get(url)
     return response.status_code, response.text
 end
@@ -35,12 +46,11 @@ end
 function api.getPackage(packageLabel, packageVersion)
     cprint("Searching for \"" .. packageLabel .. "\" in our repository... ", true)
     local packageUrl = vulcanoUrl() .. "/package/" .. packageLabel
-    local apiUrl = packageUrl
     if (packageVersion) then
-        apiUrl = packageUrl .. "/" .. packageVersion
+        packageUrl = packageUrl .. "/" .. packageVersion
     end
-    dprint(apiUrl)
-    local status, response = get(apiUrl)
+    dprint(packageUrl)
+    local status, response = get(packageUrl)
     cprint("done.")
     return status, response
 end
@@ -48,11 +58,10 @@ end
 ---@param packageLabel string
 ---@param packageVersion string
 function api.getUpdate(packageLabel, packageVersion)
-    cprint("Searching update for \"" .. packageLabel .. "\" in our repository... ", true)
-    local packageUrl = vulcanoUrl() .. "/update" .. packageLabel
-    local apiUrl = packageUrl .. "/" .. packageVersion
-    dprint(apiUrl)
-    local status, response = get(apiUrl)
+    cprint("Searching update for \"" .. packageLabel .. "-" .. packageVersion .. "\" in our repository... ", true)
+    local packageUrl = vulcanoUrl() .. "/update/" .. packageLabel .. "/" .. packageVersion
+    dprint(packageUrl)
+    local status, response = get(packageUrl)
     cprint("done.")
     return status, response
 end
--- modules/constants.lua
+++ modules/constants.lua
@@ -2,9 +2,10 @@ local constants = {}
 
 constants.mercuryVersion = "3.0.0"
 constants.xd3CmdLine = "xdelta3 -d -s \"%s\" \"%s\" \"%s\""
+constants.xd3CmdDiffLine = "xdelta3 -v -f -e -s \"%s\" \"%s\" \"%s\""
 constants.latestReleaseApi = "https://api.github.com/repos/Sledmine/Mercury/releases/latest"
 constants.gitHubReleases = "https://github.com/Sledmine/Mercury/releases/%s/"
-constants.mercuryWeb = "http://mercury.shadowmods.net"
+constants.mercuryWeb = "https://mercury.shadowmods.net"
 constants.repositoryHost = "genesis.shadowmods.net"
 --constants.vulcanoPath = ""
 constants.mapRepositoryDownload = "http://maps.halonet.net/maps/%s.zip"

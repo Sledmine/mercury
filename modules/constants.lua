1339c6e5915b029468028266916efe01c71d3163
Sledmine <40512223+Sledmine@users.noreply.github.com>
2021-11-05 14:58:41 -0700

    update api host

--- modules/constants.lua
+++ modules/constants.lua
@@ -6,7 +6,7 @@ constants.xd3CmdDiffLine = "xdelta3 -v -f -e -s \"%s\" \"%s\" \"%s\""
 constants.latestReleaseApi = "https://api.github.com/repos/Sledmine/Mercury/releases/latest"
 constants.gitHubReleases = "https://github.com/Sledmine/Mercury/releases/%s/"
 constants.mercuryWeb = "https://mercury.shadowmods.net"
-constants.repositoryHost = "genesis.shadowmods.net"
+constants.repositoryHost = "vulcano.shadowmods.net"
 --constants.vulcanoPath = ""
 constants.mapRepositoryDownload = "http://maps.halonet.net/maps/%s.zip"
 

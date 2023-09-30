2684f8ac9896be34c1c5fb2fd0372d7fc888d84f
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-09-30 17:53:34 -0600

    cleanup tests, update merc tests

--- modules/tests/merc.test.lua
+++ modules/tests/merc.test.lua
@@ -3,19 +3,26 @@
 -- Sledmine
 -- Tests for merc module methods
 ------------------------------------------------------------------------------
+package.path = package.path .. ";Mercury/?.lua"
+luna = require "modules.luna"
 local lu = require "luaunit"
 inspect = require "inspect"
+
 require "modules.utils"
 config = require "cli.config"
+local paths = config.paths()
 local merc = require "modules.merc"
+local unpack = merc.unpack
+local pack = merc.pack
+IsDebugModeEnabled = true
 
 testMerc = {}
 
 -- Before every unit test
 function testMerc:setUp()
-    self.unpackPackagePath = "Mercury/tests/modules/merc/unpack/package.merc"
-    self.unpackedPath = "Mercury/tests/modules/merc/unpack/unpacked/"
-    
+    self.packagePath = gpath("Mercury/tests/package.zip")
+    self.unpackedPath = gpath(paths.mercuryUnpacked, "/", "package")
+
     self.packPath = "Mercury/tests/modules/merc/pack/"
     self.packedPath = "Mercury/tests/modules/merc/pack/"
 
@@ -29,19 +36,22 @@ end
 function testMerc:testUnpack()
     delete(self.unpackedPath, true)
     createFolder(self.unpackedPath)
-    if (exists(self.unpackPackagePath)) then
-        lu.assertIsTrue(merc.unpack(self.unpackPackagePath, self.unpackedPath))
-        lu.assertIsTrue(exists(self.unpackedPath .. "/manifest.json"))
-        delete(self.unpackedPath, true)
-    else
+
+    if not exists(self.packagePath) then
         lu.fail("Input merc package does not exist")
     end
+    lu.assertIsTrue(unpack(self.packagePath, self.unpackedPath, "7z"), "Unpack method failed")
+    local manifestPath = gpath(self.unpackedPath .. "/manifest.json")
+    dprint("Manifest path: " .. manifestPath)
+    lu.assertIsTrue(exists(manifestPath))
+    delete(self.unpackedPath, true)
 end
 
 function testMerc:testPack()
     delete(self.packedPath)
-    if(exists(self.packPath)) then
-        lu.assertIsTrue(merc.pack(self.packPath, self.packedPath))
+
+    if exists(self.packPath) then
+        lu.assertIsTrue(pack(self.packPath, self.packedPath))
         lu.assertIsTrue(exists(self.packedPath))
         delete(self.packedPath .. "unpacked-1.0.0.zip")
     else
@@ -54,8 +64,10 @@ function testMerc:testDiff()
     delete(self.diffUnpackedFolder)
     delete(self.diffPackageFolder .. "test-1.0.1.mercu")
     lu.assertIsTrue(merc.diff(self.oldPackagePath, self.newPackagePath, self.diffPackageFolder))
-    lu.assertIsTrue(merc.unpack(self.diffPackageFolder .. "test-1.0.1.mercu", self.diffUnpackedFolder))
-    lu.assertEquals(SHA256(self.diffUnpackedFolder .. "game-maps/newfile.map"), SHA256(self.diffPackageFolder .. "newfile.map"))
+    lu.assertIsTrue(merc.unpack(self.diffPackageFolder .. "test-1.0.1.mercu",
+                                self.diffUnpackedFolder))
+    lu.assertEquals(SHA256(self.diffUnpackedFolder .. "game-maps/newfile.map"),
+                    SHA256(self.diffPackageFolder .. "newfile.map"))
     delete(self.diffUnpackedFolder)
     delete(self.diffPackageFolder .. "test-1.0.1.mercu")
 end
@@ -65,7 +77,7 @@ local function runTests()
     runner:runSuite()
 end
 
-if (not arg) then
+if not arg then
     return runTests
 else
     runTests()
